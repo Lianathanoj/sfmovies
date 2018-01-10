04d0976be6ba250fccf9c840870d82230cf4f10f
Jonathan Lian <jonathan@lob.com>
2018-01-10 13:48:59 -0800

    feat(database): setup db config

--- config/test.js
+++ config/test.js
@@ -7,4 +7,9 @@ module.exports = {
   DB_PORT: 5432,
   DB_USER: 'sfmovies_user',
   PORT: 3000
+  DB_HOST: 'localhost',
+  DB_NAME: 'sfmovies_test',
+  DB_PASSWORD: '',
+  DB_PORT: 5432,
+  DB_USER: 'sfmovies_user'
 };
