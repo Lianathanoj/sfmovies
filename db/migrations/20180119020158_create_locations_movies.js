1324fb9e16a6f00acf08d032a37270f05a83aa00
Jonathan Lian <jonathan@lob.com>
2018-01-23 10:27:48 -0800

    chore(migrations): changed table name to be compatible with bookshelf

--- db/migrations/20180119020158_create_locations_movies.js
+++ db/migrations/20180119020158_create_locations_movies.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.up = (Knex) => {
-  return Knex.schema.createTable('movies_locations', (table) => {
+  return Knex.schema.createTable('locations_movies', (table) => {
     table.increments('id').primary();
     table.integer('movie_id').references('movies.id');
     table.integer('location_id').references('locations.id');
@@ -9,5 +9,5 @@ exports.up = (Knex) => {
 };
 
 exports.down = (Knex) => {
-  return Knex.schema.dropTable('movies_locations');
+  return Knex.schema.dropTable('locations_movies');
 };
